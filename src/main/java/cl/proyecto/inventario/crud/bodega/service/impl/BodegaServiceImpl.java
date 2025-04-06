734ff01d576126f72d84d608f6f03841124e16f1
camilaeri <cam.erices@duocuc.cl>
2025-04-06 00:37:46 -0400

    pendiente
    no terminado, espera siguiente pull

--- src/main/java/cl/proyecto/inventario/crud/bodega/service/impl/BodegaServiceImpl.java
+++ src/main/java/cl/proyecto/inventario/crud/bodega/service/impl/BodegaServiceImpl.java
@@ -1,5 +1,29 @@
 package cl.proyecto.inventario.crud.bodega.service.impl;
 
+import java.util.List;
+
+import cl.proyecto.inventario.crud.bodega.dto.BodegaDTO;
+
 public class BodegaServiceImpl {
+
+    public BodegaDTO insert(BodegaDTO bodega) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'insert'");
+    }
+
+    public BodegaDTO getById(int id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getById'");
+    }
+
+    public List<BodegaDTO> getAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAll'");
+    }
+
+    public BodegaDTO update(int id, BodegaDTO producto) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
 
 }
