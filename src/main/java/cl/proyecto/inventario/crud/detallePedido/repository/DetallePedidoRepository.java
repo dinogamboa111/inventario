5465f43f39620822ed0d2203468444500bddc21d
dinogamboa111 <din.gamboa@duocuc.cl>
2025-04-09 18:29:27 -0400

    correcion904

--- src/main/java/cl/proyecto/inventario/crud/detallePedido/repository/DetallePedidoRepository.java
+++ src/main/java/cl/proyecto/inventario/crud/detallePedido/repository/DetallePedidoRepository.java
@@ -1,10 +1,10 @@
 
-package cl.proyecto.inventario.crud.detallePedido.repository;
+package cl.proyecto.inventario.crud.detallepedido.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import cl.proyecto.inventario.crud.detallePedido.dto.DetallePedidoDTO;
+import cl.proyecto.inventario.crud.detallepedido.dto.DetallePedidoDTO;
 
 @Repository
 public interface DetallePedidoRepository extends CrudRepository<DetallePedidoDTO, Integer> {
